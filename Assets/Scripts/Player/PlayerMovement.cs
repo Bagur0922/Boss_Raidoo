6e19da35eb12cae4db2b88dc72de6228763d96d0
PlasterApple <onyxcu6e@gmail.com>
2022-12-25 20:33:51 +0900

    디버그 로그 일부 제거

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -106,8 +106,6 @@ public class PlayerMovement : MonoBehaviour
     }
     void roll()
     { 
-        Debug.Log("roll");
-        
         if (canDash && !isAttacking)
         //스페이스바를 눌렀고 대쉬 가능하며 공격중이지 않은가
         {
@@ -122,10 +120,8 @@ public class PlayerMovement : MonoBehaviour
     }
     void Attack()
     {
-        
         if (canAttack)
         {
-            Debug.Log("Attack");
             if (xmove != 0)
             {
                 attackmove = true;
@@ -181,8 +177,6 @@ public class PlayerMovement : MonoBehaviour
     }
     IEnumerator ghost(float time)
     {
-        Debug.Log("ghost");
-        
         int current = 0;
         alghost++;
         current = alghost;
@@ -197,8 +191,6 @@ public class PlayerMovement : MonoBehaviour
     }
     IEnumerator comeback(float time)
     {
-        Debug.Log("comeback");
-        
         int current = 0;
         alback++;
         current = alback;
@@ -218,8 +210,6 @@ public class PlayerMovement : MonoBehaviour
     }
     public IEnumerator counter()
     {
-        Debug.Log("counter");
-        
         damge = false;
         counteranyaction = false;
         rb.velocity = new Vector2(0, 0);
